90d895ed3388eec85f48f4c68b76cb357cc35487
awisu2 <awisu2@gmail.com>
2017-01-29 17:40:09 +0900

    変換パターンの追加

--- time/time.go
+++ time/time.go
@@ -14,3 +14,59 @@ const (
 func StringMysql(t time.Time) string {
 	return fmt.Sprintf("%v/%v/%v %v:%v:%v", t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute(), t.Second())
 }
+
+// parse mysql format
+func ParseMySql(text string) (t time.Time, err error) {
+	return time.Parse(MYSQL, text)
+}
+
+// parce and convert format
+func ConvertMysql(text string, f func(t time.Time) string) (date string, err error) {
+	t, err := ParseMySql(text)
+	if err != nil {
+		return
+	}
+	return f(t), nil
+}
+
+// create YYYY/MM/DD HH:MM by mysql format strinig
+func MysqlToYYYYMMDDHHMM(text string) (date string, err error) {
+	return ConvertMysql(text, func(t time.Time) string {
+		return fmt.Sprintf("%v/%v/%v %v:%v", t.Year(), int(t.Month()), t.Day(), t.Hour(), t.Minute())
+	})
+}
+
+// create MM/DD HH:MM by mysql format strinig
+func MysqlToMMDDHHMM(text string) (date string, err error) {
+	return ConvertMysql(text, func(t time.Time) string {
+		return fmt.Sprintf("%v/%v %v:%v", int(t.Month()), t.Day(), t.Hour(), t.Minute())
+	})
+}
+
+// create YYYY/MM/DD by mysql format strinig
+func MysqlToYYYYMMDD(text string) (date string, err error) {
+	return ConvertMysql(text, func(t time.Time) string {
+		return fmt.Sprintf("%v/%v/%v", t.Year(), int(t.Month()), t.Day())
+	})
+}
+
+// create MM/DD by mysql format strinig
+func MysqlToMMDD(text string) (date string, err error) {
+	return ConvertMysql(text, func(t time.Time) string {
+		return fmt.Sprintf("%v/%v", int(t.Month()), t.Day())
+	})
+}
+
+// create YYYY年MM月DD日 by mysql format strinig
+func MysqlToYYYYMMDDJP(text string) (date string, err error) {
+	return ConvertMysql(text, func(t time.Time) string {
+		return fmt.Sprintf("%v年%v月%v日", t.Year(), int(t.Month()), t.Day())
+	})
+}
+
+// create MM月DD日 by mysql format strinig
+func MysqlToMMDDJP(text string) (date string, err error) {
+	return ConvertMysql(text, func(t time.Time) string {
+		return fmt.Sprintf("%v月%v日", int(t.Month()), t.Day())
+	})
+}
